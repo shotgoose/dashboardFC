0f4d0ad99e7604df4567c6f9452158485d0d59cf
rsfijen <rsfijen@ncsu.edu>
2025-09-14 17:42:02 -0400

    Update main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+const data = {
+    rpm: 0,
+    speed: 0,
+    coolantTemp: 0,
+    oilPressure: 0,
+    voltage: 0,
+    fuelLevel: 0,
+    outsideTemp: 0,  
+    illumination: false,
+    rightTurnSignal: false,
+    leftTurnSignal: false,
+    hazards: false,
+    highBeam: false,
+}
